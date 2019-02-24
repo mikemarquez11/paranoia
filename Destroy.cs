6c1e0f5a7fd8f857b3a971156c49d20eeb71167f
Mike Marquez <mike.marquez11@gmail.com>
2019-02-24 10:42:02 -0600

    Destroy script

---
+++ Destroy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider objecto)
+    {
+        if (objecto.gameObject.tag == "Estrella")
+        {
+            Destroy(objecto.gameObject);
+            GameManager.stars++;
+        }
+    }
+}

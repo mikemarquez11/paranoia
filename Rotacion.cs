b7c136e73058fb196d8c03b578663646b9e604f1
Mike Marquez <mike.marquez11@gmail.com>
2019-02-24 10:39:29 -0600

    Rotation script

---
+++ Rotacion.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotacion : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (new Vector3(180,0,0), Time.deltaTime, Space.World);
+		//transform.Rotate(0,180,0);
+	}
+}

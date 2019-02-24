b1b33696be3d56b14d671f5010f13bc4c2d73309
Mike Marquez <mike.marquez11@gmail.com>
2019-02-24 10:40:36 -0600

    Game Manager Script

---
+++ GameManager.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public static class GameManager {
+	public static int stars = 0;
+    public static int muertes = 3;
+    public static int keys = 0;
+}
